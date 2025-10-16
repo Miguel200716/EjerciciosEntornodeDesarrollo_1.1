3c1cf0f9b51b2a06174a3d160a89d2087e687b67
Miguel <elaquarishd@gmail.com>
2025-10-16 09:51:52 +0200

    ej1

---
+++ ej1.js
@@ -0,0 +1,20 @@
+/*
+Lee desde entrada dos variables numéricas A y B. Con ellas se pide realizar un algoritmo que intercambie
+ los valores de ambas variables y muestre cuánto valen al final las dos variables.
+ */
+
+let prompt = require('prompt-sync')();
+
+let a = prompt("introduzca a");
+
+//type of a === "string";
+a = Number(a);
+//type of a === "Number";
+let b = prompt("introduzca b");
+b = Number(b);
+
+let c = a;
+a=b;
+b=c;
+
+console.log(`El valor de a = ${a} y b = ${b}`);
